9294e4c6b3e21e2f20f2c0eb50cc24181d301208
Ian-Moffett <hscorp99906@gmail.com>
2022-02-20 21:24:13 -0500

    Changed alert message.

--- src/main.c
+++ src/main.c
@@ -26,7 +26,7 @@ int main(int argc, char* argv[]) {
     } 
 
     if (argc < 2) {
-        printf("Usage: klc <filename>\n");
+        printf("Usage: %s <filename>\n", argv[0]);
         exit(0);
     }
 
